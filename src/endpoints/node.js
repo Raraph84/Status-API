c23dcc2afee6df5f7aabb99b4e3f845cb6cc64a5
Raraph84 <contact@raraph.fr>
2022-09-11 19:44:15 +0200

    Add node info endpoint ; update packages

---
+++ src/endpoints/node.js
@@ -0,0 +1,42 @@
+const { query } = require("raraph84-lib");
+
+/**
+ * @param {import("raraph84-lib/src/Request")} request 
+ * @param {import("mysql").Pool} database 
+ */
+module.exports.run = async (request, database) => {
+
+    let node;
+    try {
+        node = (await query(database, "SELECT * FROM Nodes WHERE Node_ID=?", [request.urlParams.id]))[0];
+    } catch (error) {
+        request.end(500, "Internal server error");
+        console.log(`SQL Error - ${__filename} - ${error}`);
+        return;
+    }
+
+    if (!node) {
+        request.end(400, "This node does not exist");
+        return;
+    }
+
+    let lastStatus;
+    try {
+        lastStatus = (await query(database, "SELECT * FROM Nodes_Events WHERE Node_ID=? ORDER BY Minute DESC LIMIT 1", [node.Node_ID]))[0];
+    } catch (error) {
+        request.end(500, "Internal server error");
+        console.log(`SQL Error - ${__filename} - ${error}`);
+        return;
+    }
+
+    request.end(200, {
+        id: node.Node_ID,
+        name: node.Name,
+        online: !!lastStatus && !!lastStatus.Online
+    });
+}
+
+module.exports.infos = {
+    path: "/nodes/:id",
+    method: "GET"
+}
