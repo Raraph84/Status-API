35992fdf6537d6036f40d0dedb1478d7573de844
Raraph84 <contact@raraph.fr>
2024-10-09 15:00:25 +0200

    Add POST /pages ; Rename GET /checkers/:checkerId/services file

---
+++ src/endpoints/pages/createPage.js
@@ -0,0 +1,78 @@
+/**
+ * @param {import("raraph84-lib/src/Request")} request 
+ * @param {import("mysql2/promise").Pool} database 
+ */
+module.exports.run = async (request, database) => {
+
+    if (!request.jsonBody) {
+        request.end(400, "Invalid JSON");
+        return;
+    }
+
+    if (typeof request.jsonBody.shortName === "undefined") {
+        request.end(400, "Missing short name");
+        return;
+    }
+
+    if (typeof request.jsonBody.shortName !== "string") {
+        request.end(400, "Short name must be a string");
+        return;
+    }
+
+    if (typeof request.jsonBody.title === "undefined") {
+        request.end(400, "Missing title");
+        return;
+    }
+
+    if (typeof request.jsonBody.title !== "string") {
+        request.end(400, "Title must be a string");
+        return;
+    }
+
+    if (typeof request.jsonBody.url === "undefined") {
+        request.end(400, "Missing url");
+        return;
+    }
+
+    if (typeof request.jsonBody.url !== "string") {
+        request.end(400, "Url must be a string");
+        return;
+    }
+
+    if (typeof request.jsonBody.logoUrl === "undefined") {
+        request.end(400, "Missing logo url");
+        return;
+    }
+
+    if (typeof request.jsonBody.logoUrl !== "string") {
+        request.end(400, "Logo url must be a string");
+        return;
+    }
+
+    if (typeof request.jsonBody.domain === "undefined") {
+        request.end(400, "Missing domain");
+        return;
+    }
+
+    if (typeof request.jsonBody.domain !== "string" && request.jsonBody.domain !== null) {
+        request.end(400, "Domain must be a string or null");
+        return;
+    }
+
+    let pageId;
+    try {
+        pageId = (await database.query("INSERT INTO pages (short_name, title, url, logo_url, domain) VALUES (?, ?, ?, ?, ?)", [request.jsonBody.shortName, request.jsonBody.title, request.jsonBody.url, request.jsonBody.logoUrl, request.jsonBody.domain]))[0].insertId;
+    } catch (error) {
+        request.end(500, "Internal server error");
+        console.log(`SQL Error - ${__filename} - ${error}`);
+        return;
+    }
+
+    request.end(200, { id: pageId });
+}
+
+module.exports.infos = {
+    path: "/pages",
+    method: "POST",
+    requiresAuth: true
+}
