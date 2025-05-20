9977c858d910eb074deff7d2b416daee8ee2f992
Raraph84 <contact@raraph.fr>
2025-05-20 22:04:31 +0200

    Move old uptimes/reponse times logics to funcs

--- src/endpoints/services/serviceResponseTimes.ts
+++ src/endpoints/services/serviceResponseTimes.ts
@@ -20,7 +20,7 @@ export const run = async (request: Request, database: Pool) => {
     const day = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
     const startDay = day - 30 * 3 + 1;
 
-    if (service.type !== "server") {
+    const getOldResponseTimes = async () => {
         let statuses;
         try {
             [statuses] = await database.query<RowDataPacket[]>(
@@ -28,16 +28,21 @@ export const run = async (request: Request, database: Pool) => {
                 [service.id, config.dataCheckerId, startDay]
             );
         } catch (error) {
-            request.end(500, "Internal server error");
             console.log(`SQL Error - ${__filename} - ${error}`);
-            return;
+            throw error;
         }
 
         const getTodayResponseTime = async () => {
-            const [statuses] = await database.query<RowDataPacket[]>(
-                "SELECT * FROM services_statuses WHERE service_id=? && checker_id=? && minute>=? && minute<?",
-                [service.id, config.dataCheckerId, day * 24 * 60, (day + 1) * 24 * 60]
-            );
+            let statuses;
+            try {
+                [statuses] = await database.query<RowDataPacket[]>(
+                    "SELECT * FROM services_statuses WHERE service_id=? && checker_id=? && minute>=? && minute<?",
+                    [service.id, config.dataCheckerId, day * 24 * 60, (day + 1) * 24 * 60]
+                );
+            } catch (error) {
+                console.log(`SQL Error - ${__filename} - ${error}`);
+                throw error;
+            }
 
             const onlineStatuses = statuses.filter((status) => status.online);
             return onlineStatuses.length > 0
@@ -48,15 +53,7 @@ export const run = async (request: Request, database: Pool) => {
                 : null;
         };
 
-        let todayResponseTime;
-        try {
-            todayResponseTime = await getTodayResponseTime();
-        } catch (error) {
-            request.end(500, "Internal server error");
-            console.log(`SQL Error - ${__filename} - ${error}`);
-            return;
-        }
-
+        const todayResponseTime = await getTodayResponseTime();
         statuses.push({ day, response_time: todayResponseTime } as RowDataPacket);
 
         const responseTimes = [];
@@ -67,41 +64,54 @@ export const run = async (request: Request, database: Pool) => {
             });
         }
 
-        request.end(200, { responseTimes });
-    } else {
-        let smokeping;
+        return responseTimes;
+    };
+
+    if (service.type !== "server") {
+        let responseTimes;
         try {
-            [smokeping] = await database.query<RowDataPacket[]>(
-                "SELECT * FROM services_smokeping WHERE checker_id=? AND service_id=? AND start_time>=?",
-                [config.dataCheckerId, service.id, startDay * 24 * 60 * 6]
-            );
+            responseTimes = await getOldResponseTimes();
         } catch (error) {
             request.end(500, "Internal server error");
-            console.log(`SQL Error - ${__filename} - ${error}`);
             return;
         }
 
-        const responseTimes = [];
-        for (let currentDay = startDay; currentDay <= day; currentDay++) {
-            const startTime = currentDay * 24 * 60 * 6;
-            const endTime = (currentDay + 1) * 24 * 60 * 6;
+        request.end(200, { responseTimes });
+        return;
+    }
 
-            let sum = 0;
-            let count = 0;
-            for (const ping of smokeping) {
-                if (ping.start_time < startTime || !ping.med_response_time) continue;
-                if (ping.start_time >= endTime) break;
-                const c = ping.sent - (ping.lost ?? 0);
-                sum += ping.med_response_time * c;
-                count += c;
-            }
+    let smokeping;
+    try {
+        [smokeping] = await database.query<RowDataPacket[]>(
+            "SELECT * FROM services_smokeping WHERE checker_id=? AND service_id=? AND start_time>=?",
+            [config.dataCheckerId, service.id, startDay * 24 * 60 * 6]
+        );
+    } catch (error) {
+        request.end(500, "Internal server error");
+        console.log(`SQL Error - ${__filename} - ${error}`);
+        return;
+    }
 
-            const responseTime = count > 0 ? Math.round(sum / count) / 100 : null;
-            responseTimes.push({ day: currentDay, responseTime });
+    const responseTimes = [];
+    for (let currentDay = startDay; currentDay <= day; currentDay++) {
+        const startTime = currentDay * 24 * 60 * 6;
+        const endTime = (currentDay + 1) * 24 * 60 * 6;
+
+        let sum = 0;
+        let count = 0;
+        for (const ping of smokeping) {
+            if (ping.start_time < startTime || !ping.med_response_time) continue;
+            if (ping.start_time >= endTime) break;
+            const c = ping.sent - (ping.lost ?? 0);
+            sum += ping.med_response_time * c;
+            count += c;
         }
 
-        request.end(200, { responseTimes });
+        const responseTime = count > 0 ? Math.round(sum / count) / 100 : null;
+        responseTimes.push({ day: currentDay, responseTime });
     }
+
+    request.end(200, { responseTimes });
 };
 
 export const infos = {
--- src/endpoints/services/serviceUptimes.ts
+++ src/endpoints/services/serviceUptimes.ts
@@ -20,7 +20,7 @@ export const run = async (request: Request, database: Pool) => {
     const day = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
     const startDay = day - 30 * 3 + 1;
 
-    if (service.type !== "server") {
+    const getOldUptimes = async () => {
         let statuses;
         try {
             [statuses] = await database.query<RowDataPacket[]>(
@@ -28,16 +28,21 @@ export const run = async (request: Request, database: Pool) => {
                 [service.id, config.dataCheckerId, day - 30 * 3 + 1]
             );
         } catch (error) {
-            request.end(500, "Internal server error");
             console.log(`SQL Error - ${__filename} - ${error}`);
-            return;
+            throw error;
         }
 
         const getTodayUptime = async () => {
-            const [statuses] = await database.query<RowDataPacket[]>(
-                "SELECT * FROM services_statuses WHERE service_id=? && checker_id=? && minute>=? && minute<?",
-                [service.id, config.dataCheckerId, day * 24 * 60, (day + 1) * 24 * 60]
-            );
+            let statuses;
+            try {
+                [statuses] = await database.query<RowDataPacket[]>(
+                    "SELECT * FROM services_statuses WHERE service_id=? && checker_id=? && minute>=? && minute<?",
+                    [service.id, config.dataCheckerId, day * 24 * 60, (day + 1) * 24 * 60]
+                );
+            } catch (error) {
+                console.log(`SQL Error - ${__filename} - ${error}`);
+                throw error;
+            }
 
             const onlineStatuses = statuses.filter((status) => status.online);
             return statuses.length > 0
@@ -45,15 +50,7 @@ export const run = async (request: Request, database: Pool) => {
                 : null;
         };
 
-        let todayUptime;
-        try {
-            todayUptime = await getTodayUptime();
-        } catch (error) {
-            request.end(500, "Internal server error");
-            console.log(`SQL Error - ${__filename} - ${error}`);
-            return;
-        }
-
+        const todayUptime = await getTodayUptime();
         statuses.push({ day, uptime: todayUptime } as RowDataPacket);
 
         const uptimes = [];
@@ -64,40 +61,53 @@ export const run = async (request: Request, database: Pool) => {
             });
         }
 
-        request.end(200, { uptimes });
-    } else {
-        let smokeping;
+        return uptimes;
+    };
+
+    if (service.type !== "server") {
+        let uptimes;
         try {
-            [smokeping] = await database.query<RowDataPacket[]>(
-                "SELECT * FROM services_smokeping WHERE checker_id=? AND service_id=? AND start_time>=?",
-                [config.dataCheckerId, service.id, startDay * 24 * 60 * 6]
-            );
+            uptimes = await getOldUptimes();
         } catch (error) {
             request.end(500, "Internal server error");
-            console.log(`SQL Error - ${__filename} - ${error}`);
             return;
         }
 
-        const uptimes = [];
-        for (let currentDay = startDay; currentDay <= day; currentDay++) {
-            const startTime = currentDay * 24 * 60 * 6;
-            const endTime = (currentDay + 1) * 24 * 60 * 6;
+        request.end(200, { uptimes });
+        return;
+    }
 
-            let durations = 0;
-            let downs = 0;
-            for (const ping of smokeping) {
-                if (ping.start_time < startTime) continue;
-                if (ping.start_time >= endTime) break;
-                durations += ping.duration;
-                downs += ping.downs ?? 0;
-            }
+    let smokeping;
+    try {
+        [smokeping] = await database.query<RowDataPacket[]>(
+            "SELECT * FROM services_smokeping WHERE checker_id=? AND service_id=? AND start_time>=?",
+            [config.dataCheckerId, service.id, startDay * 24 * 60 * 6]
+        );
+    } catch (error) {
+        request.end(500, "Internal server error");
+        console.log(`SQL Error - ${__filename} - ${error}`);
+        return;
+    }
 
-            const uptime = durations > 0 ? Math.round(((durations - downs) / durations) * 100 * 1000) / 1000 : null;
-            uptimes.push({ day: currentDay, uptime });
+    const uptimes = [];
+    for (let currentDay = startDay; currentDay <= day; currentDay++) {
+        const startTime = currentDay * 24 * 60 * 6;
+        const endTime = (currentDay + 1) * 24 * 60 * 6;
+
+        let durations = 0;
+        let downs = 0;
+        for (const ping of smokeping) {
+            if (ping.start_time < startTime) continue;
+            if (ping.start_time >= endTime) break;
+            durations += ping.duration;
+            downs += ping.downs ?? 0;
         }
 
-        request.end(200, { uptimes });
+        const uptime = durations > 0 ? Math.round(((durations - downs) / durations) * 100 * 1000) / 1000 : null;
+        uptimes.push({ day: currentDay, uptime });
     }
+
+    request.end(200, { uptimes });
 };
 
 export const infos = {
